ffd431bca1a20b109a8b770d5f9d04457544721a
Carlos Valeriano <carlosvaleriano45@yahoo.com>
2020-01-27 16:25:47 -0600

    Added text highlighting and more tabs.

--- Scraper/OutputForm.cs
+++ Scraper/OutputForm.cs
@@ -13,6 +13,17 @@ namespace Scraper
 {
     public partial class OutputForm : Form
     {
+        private struct Candidate
+        {
+            public int keywordCount;
+            public KeyValuePair<string, string> text;
+            public Candidate(KeyValuePair<string, string> subChapter)
+            {
+                keywordCount = 0;
+                text = subChapter;
+            }
+        }
+
         public OutputForm(List<string> keyPhrases)
         {
             InitializeComponent();
@@ -20,10 +31,9 @@ namespace Scraper
             //key phrases: get the input from edit text box and split by line
             // look to see which sub chapter has the most key phrases
 
-            //string[] keywords = inputText.Split(' ');
-            int keywordCount = 0;
+            //int keywordCount = 0;
             int highestKeywordCount = 0;
-            KeyValuePair<string, string> winner = new KeyValuePair<string, string>();
+            Stack<Candidate> candidates = new Stack<Candidate>();
 
             Debug.WriteLine("Keyphrases:");
             foreach (string word in keyPhrases)
@@ -37,57 +47,121 @@ namespace Scraper
             {
                 foreach (KeyValuePair<string, string> section in selectedChapter.subChapters)
                 {
+                    Candidate candidate = new Candidate(section);
+
                     Debug.WriteLine(section.Key + " contains these keyphrases:");
+
+                    string[] words = section.Value.Split(' ');
+
                     foreach (string phrase in keyPhrases)
                     {
+                        //note: seems to grab phrases within words like "crust" in "encrusted"
                         if (section.Value.Contains(phrase))
                         {
                             Debug.Write(phrase + " ");
-                            keywordCount++;
+                            candidate.keywordCount++;
                         }
                     }
                     Debug.WriteLine("");
 
-                    //section with most keywords
-                    if (keywordCount > highestKeywordCount)
+                    //stack keeps track of who's winning
+                    if (candidate.keywordCount >= highestKeywordCount)
                     {
-                        winner = section;
-                        highestKeywordCount = keywordCount;
+                        Debug.WriteLine("Highest keyword count was: " + highestKeywordCount);
+                        candidates.Push(candidate);
+                        highestKeywordCount = candidate.keywordCount;
+                        Debug.WriteLine("Now it was updated to: " + highestKeywordCount);
                     }
 
-                    keywordCount = 0;
+                    //keywordCount = 0;
                 }
             }
 
-            Debug.WriteLine("Winner: " + winner.Key);
+            // the winners are the ones with the highest keyword count
+            List<Candidate> winners = new List<Candidate>();
 
-            RichTextBox newText = new RichTextBox();
+            while (candidates.Count != 0)
+            {
+                if (candidates.Peek().keywordCount == highestKeywordCount)
+                {
+                    Debug.WriteLine("pushed");
+                    winners.Add(candidates.Pop());
+                }
+                else
+                {
+                    candidates.Pop();
+                }
+            }
 
-            newText.MinimumSize = new Size(500, 200);
+            if (highestKeywordCount != 0)
+            {
+                Debug.WriteLine("Winners: ");
+                foreach (Candidate winner in winners)
+                {
+                    // each winner should have its own tab and text box
+                    Debug.Write(winner.text.Key + " ");
 
-            newText.Multiline = true;
-            newText.ReadOnly = true;
-            newText.ScrollBars = RichTextBoxScrollBars.Vertical;
-            newText.Text = winner.Value;
+                    TabPage page = new TabPage(winner.text.Key);
 
-            //newText.SelectionColor = Color.Yellow;
-            
-            //foreach (string phrase in keyPhrases)
-            //{
-            //    int index = newText.Find(phrase);
-            //    newText.SelectionStart = index;
-            //    Debug.WriteLine("start index: " + index);
-            //    newText.SelectionLength = index + phrase.Length;
-            //    newText.Select();
-            //    //newText.Select(index, index + phrase.Length);
-            //}
+                    RichTextBox textBox = new RichTextBox();
+                    textBox.MinimumSize = new Size(500, 200);
+                    textBox.Multiline = true;
+                    textBox.ReadOnly = true;
+                    textBox.ScrollBars = RichTextBoxScrollBars.Vertical;
+                    textBox.Text = winner.text.Value;
 
-            TabPage tabPage = new TabPage(winner.Key);
-            tabPage.Name = winner.Key;
-            tabPage.Controls.Add(newText);
-            outputTabs.Controls.Add(tabPage);
+                    HighlightText(keyPhrases, textBox);
+
+                    page.Name = winner.text.Key;
+                    page.Controls.Add(textBox);
+                    outputTabs.Controls.Add(page);
+                }
+            }
+
+
+            //RichTextBox box = new RichTextBox();
+
+            //box.MinimumSize = new Size(500, 200);
+
+            //box.Multiline = true;
+            //box.ReadOnly = true;
+            //box.ScrollBars = RichTextBoxScrollBars.Vertical;
+            //box.Text = winner.Value;
+
+            //TabPage tabPage = new TabPage(winner.Key);
+            //tabPage.Name = winner.Key;
+            //tabPage.Controls.Add(box);
+
         }
 
+        private static void HighlightText(List<string> keyPhrases, RichTextBox box)
+        {
+            foreach (string phrase in keyPhrases)
+            {
+                int startIndex = 0;
+                while (startIndex < box.TextLength)
+                {
+                    //find the next phrase within the loop
+                    int phraseStartIndex = box.Find(phrase, startIndex, RichTextBoxFinds.None);
+
+                    //highlight if there are any words found, -1 means nothing was found
+                    if (phraseStartIndex != -1)
+                    {
+                        //Highlight text 
+                        box.SelectionStart = phraseStartIndex;
+                        box.SelectionLength = phrase.Length;
+                        box.SelectionBackColor = Color.Yellow;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    //start searching at the end of the word that was just found
+                    startIndex += phraseStartIndex + phrase.Length;
+                }
+            }
+        }
 
         private void tab1_Click(object sender, EventArgs e)
         {
